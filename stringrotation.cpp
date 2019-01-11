bc48b560a0f6c7a62b2e8e7e29f1c480d5a09ddb
himalay560 <46533179+himalay560@users.noreply.github.com>
2019-01-11 19:45:58 +0530

    Add files via upload

---
+++ stringrotation.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n,x,j=0;
+    cin>>n;
+    char** s;
+    s=(char**)malloc(sizeof(char**)*n);  //2d pointer array(dynamic)
+    for(int i=0;i<n;i++)
+    {
+        s[i]=(char*)malloc(100);    //declaring size of each row
+        cin>>s[i];
+    }
+    // for acyclic rotation
+    for(int i=1;i<=n;i++)
+    {
+        x=n;
+        while(x--)
+        {
+            cout<<s[j%n]<<" ";    //prints every string
+            j++;                    //increments the position of string in one line
+        }
+        j++;                        //starts from next string to print
+        cout<<endl;
+    }
+    cout<<"\n\n\t*******\n\n";
+    //for cyclic rotation
+    for(int i=0;i<n;i++)
+    {
+        x=n;
+        while(x--)
+        {
+            cout<<s[(j+(n-i))%n]<<" ";   // print the string in cyclic order
+            j++;
+        }
+        cout<<endl;
+    }
+}
