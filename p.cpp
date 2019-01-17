70ad06d9d3721ceac7e9ee1cdd422377929a251f
himalay560 <46533179+himalay560@users.noreply.github.com>
2019-01-17 12:03:47 +0530

    Update p.cpp

--- p.cpp
+++ p.cpp
@@ -38,13 +38,40 @@ int display(struct node *t)
     if(t->r!=NULL)
         display(t->r);
 }
+int search(struct node *t,int n)
+{
+    if(t->n==n){
+        cout<<"element present\n";
+        return 0;
+    }
+    if(t->n<n)
+    {
+        if(t->r!=NULL)
+            search(t->r,n);
+        else
+        {
+            cout<<"element not present\n";
+            return 0;
+        }
+    }
+    else
+    {
+        if(t->l!=NULL)
+            search(t->l,n);
+        else
+        {
+            cout<<"element not present\n";
+            return 0;
+        }
+    }
+}
 int main()
 {
     root->n=0;
     while(1)
     {
         int a;
-        cout<<"1.insert 2.display\n";
+        cout<<"1.insert 2.display 3.search\n";
         cin>>a;
         switch(a)
         {
@@ -53,7 +80,11 @@ int main()
                     insert(root,n);
                     break;
             case 2:
-                display(root);
+                display(root->r);
+                break;
+            case 3:int m;
+                cin>>m;
+                search(root,m);
                 break;
             default: exit(0);
                      break;
